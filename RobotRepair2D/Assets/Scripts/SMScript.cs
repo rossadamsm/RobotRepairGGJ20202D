62b83494584cf49d10c0bf492c47f69e35ba1786
reyvan-d <vdwesthuizenreyno@gmail.com>
2020-02-01 22:26:35 +0200

    main music

---
+++ RobotRepair2D/Assets/Scripts/SMScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMScript : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SMScript instance = null;
+
+    public AudioClip[] Sounds;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        PlayLoopFromSource();
+    }
+
+
+    public void PlaySingle(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void PlayLoop(AudioClip clip)
+    {
+        if (musicSource)
+            musicSource.Stop();
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void PlayLoopFromSource()
+    {
+        if (musicSource)
+            musicSource.Stop();
+        else
+            return;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    private AudioClip GetRandomClip(AudioClip[] audioclips)
+    {
+        int index = Random.Range(0, audioclips.Length - 1);
+        return audioclips[index];
+    }
+}
