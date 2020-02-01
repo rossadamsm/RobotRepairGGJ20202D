fae548f23ff4a14fd1ab8e7d112a4200445f351d
Kazuro <kazukix.kirigaya@gmail.com>
2020-02-01 17:07:38 +0200

    Add enemy spawner (basic) and fixed bug in towers not picking up a new target

---
+++ RobotRepair2D/Assets/Scripts/Enemy/EnemySpawnController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+    public int spawnPerWave = 3;
+
+    public float spawnIncreaseRate;
+    //Seconds
+    public float waveTime = 60f;
+
+    public GameObject enemyPrefab;
+
+    public int currentWaveCount = 1;
+    public float currentWaveTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentWaveTime <= 0) {
+            currentWaveTime = waveTime;
+            Spawn();
+        }
+
+        if (currentWaveTime > 0) {
+            currentWaveTime -= Time.deltaTime;
+        }
+    }
+
+    void Spawn() {
+        float spawnTotal = 10;
+        for (int i = 0; i < spawnTotal; i++) {
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(transform.position.x + Random.Range(1,5), transform.position.y + Random.Range(1,5), transform.position.z),Quaternion.identity);
+        }
+    }
+}
