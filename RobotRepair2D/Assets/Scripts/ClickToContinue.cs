4115b15a1ca916cd102ad63d25eb602c93f1503d
Claris Richter <clarisrichter@Clariss-MacBook-Pro.local>
2020-02-01 18:14:19 +0200

    Added transition and wrapped up menu

---
+++ RobotRepair2D/Assets/Scripts/ClickToContinue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToContinue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject transition;
+    public string destinationScene;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+            if (GameObject.FindGameObjectsWithTag("Transition").Length == 0)
+			{
+                GameObject newTrans = Instantiate(transition, Vector3.zero, Quaternion.identity);
+                Transition newTransTransition = newTrans.GetComponent<Transition>();
+                newTransTransition.fadeIn = true;
+                newTransTransition.destinationScene = destinationScene;
+			}
+        }
+        
+    }
+}
